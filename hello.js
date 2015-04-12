29da74b7c92409d484a2cfe49db99f6af1a47f9b
HaifaAlsalem <dr.csc211@hotmail.com>
2015-04-12 18:28:50 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,4 +3,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Travis!\n'); // this will FAIL travis ci lint
 }).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/')
+console.log('Server running at http://127.0.0.1:1337/');
